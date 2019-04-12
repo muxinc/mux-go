74fcfa284cd528b7ea643e25986685311445f4f4
Matt Ward <contact@mwdesigns.com>
2019-04-11 22:47:35 -0400

    more generic doc for now

--- client.go
+++ client.go
@@ -52,8 +52,7 @@ type service struct {
 	client *APIClient
 }
 
-// NewAPIClient creates a new API client. Requires a userAgent string describing your application.
-// optionally a custom http.Client to allow for advanced features such as caching.
+// NewAPIClient creates a new API client.
 func NewAPIClient(cfg *Configuration) *APIClient {
 	c := &APIClient{}
 	c.cfg = cfg
