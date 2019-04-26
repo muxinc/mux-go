0dcd12d76944cccb425b4a77c64e796837ab70b0
Phil Cluff <philcluff@googlemail.com>
2019-04-26 16:27:47 +0100

    Added Asset covereage. Adds a common test helper too, which may or may not survive.

---
+++ examples/common/lazytest.go
@@ -0,0 +1,62 @@
+package common
+
+import (
+	"fmt"
+	"os"
+	"reflect"
+)
+
+func AssertNoError(err error) {
+	if err != nil {
+		fmt.Printf("err: %s \n\n", err)
+		os.Exit(255)
+	}
+}
+
+// Copied from Testify under MIT license.
+// containsKind checks if a specified kind in the slice of kinds.
+func containsKind(kinds []reflect.Kind, kind reflect.Kind) bool {
+	for i := 0; i < len(kinds); i++ {
+		if kind == kinds[i] {
+			return true
+		}
+	}
+
+	return false
+}
+
+// Copied from Testify under MIT license.
+func  AssertNotNil(o interface{}) {
+	if o == nil {
+		fmt.Println("Object was nil!")
+		os.Exit(255)
+	}
+
+	v := reflect.ValueOf(o)
+	kind := v.Kind()
+	isNilableKind := containsKind(
+		[]reflect.Kind{
+			reflect.Chan, reflect.Func,
+			reflect.Interface, reflect.Map,
+			reflect.Ptr, reflect.Slice},
+		kind)
+
+	if isNilableKind && v.IsNil() {
+		fmt.Println("Object was nil!")
+		os.Exit(255)
+	}
+}
+
+func AssertStringEqualsValue(a string, b string) {
+	if a != b {
+		fmt.Println("Strings weren't equal!")
+		os.Exit(255)
+	}
+}
+
+func AssertStringNotEqualsValue(a string, b string) {
+	if a == b {
+		fmt.Println("Strings weren't supposed to be equal!")
+		os.Exit(255)
+	}
+}
--- examples/video/exercise-assets.go
+++ examples/video/exercise-assets.go
@@ -3,8 +3,10 @@ package main
 import (
 	"fmt"
 	"os"
+	"time"
 
 	"github.com/muxinc/mux-go"
+	"github.com/muxinc/mux-go/examples/common"
 )
 
 // Exercises all asset operations:
@@ -18,14 +20,8 @@ import (
 //   delete-asset-playback-id
 //   update-asset-mp4-support
 
-func checkError(err error) {
-	if err != nil {
-		fmt.Printf("err: %s \n\n", err)
-		os.Exit(255)
-	}
-}
-
 func main() {
+
 	// API Client Initialization
 	client := muxgo.NewAPIClient(
 		muxgo.NewConfiguration(
@@ -33,42 +29,88 @@ func main() {
 		))
 
 	// ========== create-asset ==========
-	cr, err := client.AssetsApi.CreateAsset(muxgo.CreateAssetRequest{
+	asset, err := client.AssetsApi.CreateAsset(muxgo.CreateAssetRequest{
 		Input: []muxgo.InputSettings{
 			muxgo.InputSettings{
 				Url: "https://storage.googleapis.com/muxdemofiles/mux-video-intro.mp4",
 			},
 		},
 	})
-	checkError(err)
+	common.AssertNoError(err)
+	common.AssertNotNil(asset.Data)
 	fmt.Println("create-asset OK ✅")
-	fmt.Printf("created asset with id: %s\n\n", cr.Data.Id)
 
 	// ========== list-assets ==========
 	lr, err := client.AssetsApi.ListAssets()
-	checkError(err)
+	common.AssertNoError(err)
+	common.AssertNotNil(lr.Data)
+	common.AssertStringEqualsValue(asset.Data.Id, lr.Data[0].Id)
 	fmt.Println("list-assets OK ✅")
-	fmt.Printf("loaded a list of %d assets.\n\n", len(lr.Data))
 
-	// ========== get-asset ==========
-	gr, err := client.AssetsApi.GetAsset(cr.Data.Id)
-	checkError(err)
+	// Wait for the asset to become ready
+	if asset.Data.Status != "ready" {
+		fmt.Println("    Waiting for asset to become ready...")
+		for {
+			// ========== get-asset ==========
+			gr, err := client.AssetsApi.GetAsset(asset.Data.Id)
+			common.AssertNoError(err)
+			common.AssertNotNil(gr)
+			common.AssertStringEqualsValue(asset.Data.Id, gr.Data.Id)
+			if gr.Data.Status != "ready" {
+				fmt.Println("    Asset still not ready.")
+				time.Sleep(1 * time.Second)
+			} else {
+				// ========== get-asset-input-info ==========
+				ir, err := client.AssetsApi.GetAssetInputInfo(asset.Data.Id)
+				common.AssertNoError(err)
+				common.AssertNotNil(ir.Data)
+				break
+			}
+		}
+	}
 	fmt.Println("get-asset OK ✅")
-	fmt.Printf("got the asset with id %s\n\n", gr.Data.Id)
-
-	// ========== get-asset-input-info ==========
-	ir, err := client.AssetsApi.GetAssetInputInfo(cr.Data.Id)
-	checkError(err)
 	fmt.Println("get-asset-input-info OK ✅")
-	fmt.Printf("%+v\n\n", ir.Data)
 
 	// ========== create-asset-playback-id ==========
+	capr := muxgo.CreatePlaybackIdRequest{muxgo.PUBLIC}
+	capre, err := client.AssetsApi.CreateAssetPlaybackId(asset.Data.Id, capr)
+	common.AssertNoError(err)
+	common.AssertNotNil(capre.Data)
+	common.AssertStringEqualsValue(string(capre.Data.Policy), "public")
+	fmt.Println("create-asset-playback-id OK ✅")
 
 	// ========== get-asset-playback-id ==========
+	pbre, err := client.AssetsApi.GetAssetPlaybackId(asset.Data.Id, capre.Data.Id)
+	common.AssertNoError(err)
+	common.AssertNotNil(pbre.Data)
+	common.AssertStringEqualsValue(capre.Data.Id, pbre.Data.Id)
+	fmt.Println("get-asset-playback-id OK ✅")
 
 	// ========== update-asset-mp4-support ==========
+	mp4r := muxgo.UpdateAssetMp4SupportRequest{"standard"}
+	mp4, err := client.AssetsApi.UpdateAssetMp4Support(asset.Data.Id, mp4r)
+	common.AssertNoError(err)
+	common.AssertNotNil(mp4.Data)
+	common.AssertStringEqualsValue(asset.Data.Id, mp4.Data.Id)
+	common.AssertStringEqualsValue(mp4.Data.Mp4Support, "standard")
+	fmt.Println("update-asset-mp4-support OK ✅")
 
 	// ========== delete-asset-playback-id ==========
+	err = client.AssetsApi.DeleteAssetPlaybackId(asset.Data.Id, capre.Data.Id)
+	common.AssertNoError(err)
+	dpba, err := client.AssetsApi.GetAsset(asset.Data.Id)
+	common.AssertNoError(err)
+	if len(dpba.Data.PlaybackIds) > 0 {
+		fmt.Println("List of playback IDs wasn't empty")
+		os.Exit(255)
+	}
+	fmt.Println("delete-asset-playback-id OK ✅")
 
 	// ========== delete-asset ==========
+	err = client.AssetsApi.DeleteAsset(asset.Data.Id)
+	common.AssertNoError(err)
+	_, err = client.AssetsApi.GetAsset(asset.Data.Id)
+	common.AssertNotNil(err)
+	fmt.Println("delete-asset OK ✅")
+
 }
