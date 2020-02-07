492d555798fa0e50455df87b57affae933ba5c66
Phil Cluff <philcluff@googlemail.com>
2020-02-07 18:27:20 +0000

    Remove pointer type

--- model_input_settings.go
+++ model_input_settings.go
@@ -5,12 +5,12 @@ package muxgo
 
 // Input object with additional configuration
 type InputSettings struct {
-	Url             string                        `json:"url,omitempty"`
-	OverlaySettings *InputSettingsOverlaySettings `json:"overlay_settings,omitempty"`
-	Type            string                        `json:"type,omitempty"`
-	TextType        string                        `json:"text_type,omitempty"`
-	LanguageCode    string                        `json:"language_code,omitempty"`
-	Name            string                        `json:"name,omitempty"`
-	ClosedCaptions  bool                          `json:"closed_captions,omitempty"`
-	Passthrough     string                        `json:"passthrough,omitempty"`
+	Url             string                       `json:"url,omitempty"`
+	OverlaySettings InputSettingsOverlaySettings `json:"overlay_settings,omitempty"`
+	Type            string                       `json:"type,omitempty"`
+	TextType        string                       `json:"text_type,omitempty"`
+	LanguageCode    string                       `json:"language_code,omitempty"`
+	Name            string                       `json:"name,omitempty"`
+	ClosedCaptions  bool                         `json:"closed_captions,omitempty"`
+	Passthrough     string                       `json:"passthrough,omitempty"`
 }
